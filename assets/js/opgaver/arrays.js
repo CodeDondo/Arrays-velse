34e85439470ef92d5efddd207a2d9d0d2c11c9b9
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2024-08-21 21:28:10 +0200

    Changes

--- assets/js/opgaver/arrays.js
+++ assets/js/opgaver/arrays.js
@@ -1,12 +1,14 @@
 /* arrays */
 
 //Opgave 1
-const myArray =/*din array her*/;
+
+const myArray = ['element1', 'element2', 'element3', 'element4', 'element5'];
 
 
 
 
 // opgave 2 console.logs
 
-
+const pingArray = myArray[0];
+console.log(myArray);
 
---
+++ assets/js/opgaver/test.js
@@ -0,0 +1,70 @@
+const listNameInput = document.getElementById('listNameInput')
+const listButton = document.getElementById('listButton')
+const listIndex = document.getElementById('listIndex')
+const listItemText = document.getElementById('listItemText')
+const listItemAddButton = document.getElementById('listItemAddButton')
+const listIndexRemove = document.getElementById('listIndexRemove')
+const listItemIndexRemove = document.getElementById('listItemIndexRemove')
+const listItemRemove = document.getElementById('listItemRemove')
+let myData=[]
+
+
+listButton.addEventListener('click', (event) => {
+    let myList = {
+        name: listNameInput.value,//key value pair
+        listItems: []
+    }
+
+    myData.push(myList)
+    console.table(myData)
+})
+
+listItemAddButton.addEventListener('click', (event) => {
+    myData[listIndex.value].listItems.push(listItemText.value)
+    console.table(myData)
+})
+
+listItemRemove.addEventListener('click', (event) => {
+    myData[listIndexRemove.value].listItems.splice(listItemIndexRemove.value,1)
+    console.table(myData)
+})
+
+
+// Modtager et navn string og skaber et nyt liste dataobjekt og gemmer et i myData
+function makeList(myName){
+    let myList={
+        name: myName, // key value pair
+        listItems:[]
+        }
+
+        myData.push(myList)
+        
+        console.table(myData);
+        
+}
+
+
+// Modtager et navn og opretter list item i første to do list
+
+function makeItem(myName){
+    let myListItem={
+        name:myName,
+        status:true
+    }
+
+    myData[0].listItems.push(myListItem)
+
+    console.table(myData);
+    
+}
+
+// Modtager et index for listen, og et index for item, og fjerner dette item fra listen.
+
+function removeItem(listIndex, itemIndex){
+    let myList = myData[listIndex]
+
+    console.log(myList.name);
+    
+    myList.listItems.splice(itemIndex, 1)
+
+}
